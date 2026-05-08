efcbac7ab0df5332fe1c9c25c2295f7389e8c82a
yambe <s52marry@gmail.com>
2026-05-08 15:42:02 +0900

    redeploy after env

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -11,7 +11,7 @@ export async function login() {
     const { data, error } = await supabase.auth.signInWithOAuth({
   provider: "google",
   options: {
-    redirectTo: 'http://localhost:3000/auth/callback',
+    redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
   },
 })
 
