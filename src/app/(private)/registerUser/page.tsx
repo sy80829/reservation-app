7b43ebcb709c731512f8d8c18ac9280b04179cc3
yambe <s52marry@gmail.com>
2026-05-08 17:37:51 +0900

    redeploy after env

--- src/app/(private)/registerUser/page.tsx
+++ src/app/(private)/registerUser/page.tsx
@@ -9,24 +9,46 @@ export default function Page({
 
   return (
     <form action={registerUser}>
-      <p className="text-center text-xl font-bold">会員登録</p>
+      <p className="text-center font-bold text-xl max-w-md mx-auto mt-3 mb-3 bg-white">
+        会員登録
+      </p>
 
       <div className="grid grid-cols-[auto_1fr] items-center gap-2">
         <input type="hidden" name="next" value={next ?? ''} />
 
         <p>お名前：</p>
-        <input className="border" name="name" />
+        <input
+          className="
+  border
+  rounded-lg
+  px-3
+  py-2
+  w-full
+"
+          name="name"
+        />
 
         <p>電話番号：</p>
-        <input className="border" name="tel" />
+        <input
+          className="
+  border
+  rounded-lg
+  px-3
+  py-2
+  w-full
+"
+          name="tel"
+        />
       </div>
 
-      <button
-        type="submit"
-        className="bg-[#EC1C5E] text-white hover:bg-[#d81a55] transition cursor-pointer w-full md:w-70 border rounded-3xl py-2.5 font-bold text-xl mb-3"
-      >
-        登録
-      </button>
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className="bg-[#EC1C5E] text-white hover:bg-[#d81a55] transition cursor-pointer w-full md:w-70 border rounded-3xl py-2.5 font-bold text-xl mb-3 mt-3"
+        >
+          登録
+        </button>
+      </div>
     </form>
   );
 }
