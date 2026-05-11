5ba3b34900c796956e1ea404e25a98afef18bf3a
yambe <s52marry@gmail.com>
2026-05-11 09:29:19 +0900

    変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-import Image from 'next/image';
-import Link from 'next/link';
+// src/app/page.tsx
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <>
-      <div className="max-w-screen-md">
-        <div>予約システム</div>
-        <div className="">
-          <div className="">
-            <Link href={'/top'}>ご予約はこちら</Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+export default function Page() {
+  redirect('/top');
 }
